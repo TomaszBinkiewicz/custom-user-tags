5a36023807b7d5e16ed87b75bda7fba59936f51e
Tomasz Binkiewicz <t.bin44@gmail.com>
2020-03-15 23:32:28 +0100

    altered_user_model

--- custom_user_tags_app/models.py
+++ custom_user_tags_app/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import date
+from random import randint
 
 
 class User(AbstractUser):
     birthday = models.DateField()
+    random_number = models.IntegerField()
 
     @property
     def age(self):
         today = date.today()
         delta_days = (today - self.birthday).days
         return int(delta_days / 365.242199)
+
+    def __init__(self, *args, **kwargs):
+        models.Model.__init__(self, *args, **kwargs)
+        self.random_number = randint(1, 100)
