0a34a8b007845340b76bc3f97cf22f82a91cfd70
Tomasz Binkiewicz <t.bin44@gmail.com>
2020-03-16 00:14:09 +0100

    user_listview

--- custom_user_tags/urls.py
+++ custom_user_tags/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from custom_user_tags_app.views import (UserListView,
+                                        )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users', UserListView.as_view(), name='users-list'),
 ]
--- custom_user_tags_app/views.py
+++ custom_user_tags_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import User
 
-# Create your views here.
+
+class UserListView(ListView):
+    model = User
+    paginate_by = 50
+    template_name = "custom_user_tags_app/user-list.html"
+    context_object_name = 'users'
