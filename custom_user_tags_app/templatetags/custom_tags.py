d9b8da60ddfe176fe14fea5520cf475b78e0b606
Tomasz Binkiewicz <t.bin44@gmail.com>
2020-03-16 15:36:14 +0100

    bizz_fuzz_tag

--- custom_user_tags_app/templatetags/custom_tags.py
+++ custom_user_tags_app/templatetags/custom_tags.py
@@ -8,3 +8,15 @@ def pg_13(user: object) -> str:
     if user.age > 13:
         return "allowed"
     return "blocked"
+
+
+@register.simple_tag
+def bizz_fuzz(user: object):
+    if user.random_number % 15 == 0:
+        return "BizzFuzz"
+    elif user.random_number % 5 == 0:
+        return "Fuzz"
+    elif user.random_number % 3 == 0:
+        return "Bizz"
+    else:
+        return user.random_number
